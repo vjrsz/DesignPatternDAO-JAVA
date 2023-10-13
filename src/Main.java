4633d7415d91950be67f912247cc2d28d6013fb5
vjrsz <juniornunes1008@gmail.com>
2023-10-13 16:07:34 -0300

    add Seller.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 import Models.Entities.Department;
+import Models.Entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Department p = new Department(1, "Books");
-        System.out.println(p);
+        Department d = new Department(1, "Books");
+        Seller s = new Seller(21, "Joao", "jao@gmail.com", new Date(), 3000.0, d);
+        System.out.println(d);
+        System.out.println(s);
     }
 }
