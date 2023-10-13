2e9851ca1e20079c50ca714774aa5f02b4563f12
vjrsz <juniornunes1008@gmail.com>
2023-10-13 19:31:27 -0300

    delete impl and update impl

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,7 @@ public class Main {
         Department department = new Department(2, null);
         List<Seller> sellers = sellerDao.findByDepartment(department);
         for (Seller obj : sellers){
-            System.out.println(seller);
+            System.out.println(obj);
         }
         System.out.println();
 
@@ -30,7 +30,7 @@ public class Main {
         System.out.println("Find all :");
         List<Seller> sellers2 = sellerDao.findAll();
         for (Seller obj : sellers2){
-            System.out.println(seller);
+            System.out.println(obj);
         }
 
         // insert
@@ -38,6 +38,21 @@ public class Main {
         Seller seller2 = new Seller(null, "jao", "jao@gmail.com", new Date(), 4000.00, department);
         sellerDao.insert(seller2);
         System.out.println("Inserted! New id = " + seller2.getId());
+        System.out.println();
+
+
+        // update
+        System.out.println("Update :");
+        Seller seller3 = sellerDao.findById(1);
+        seller3.setName("vjrsz");
+        sellerDao.update(seller3);
+        System.out.println("Update completed!");
+        System.out.println();
+
+        // delete
+        System.out.println("Delete :");
+        sellerDao.deleteById(3);
+        System.out.println("Delete completed!");
         System.out.println();
     }
 }
