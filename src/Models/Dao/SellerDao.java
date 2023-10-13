3caff2c2af5d44ba070119fd5289ba5736d43085
vjrsz <juniornunes1008@gmail.com>
2023-10-13 16:10:38 -0300

    add DepartmentDao | add SellerDao

---
+++ src/Models/Dao/SellerDao.java
@@ -0,0 +1,13 @@
+package Models.Dao;
+
+import Models.Entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(Seller obj);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
